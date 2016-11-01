435492fa13afb4ac215db2eb3c54cb9563886bec
snoupyrelete <drob8896@740s-pl-129.local>
2016-11-01 08:31:47 -0600

    Started the panel

--- src/graphic/view/FirstFrame.java
+++ src/graphic/view/FirstFrame.java
@@ -2,6 +2,8 @@ package graphic.view;
 
 import javax.swing.JFrame;
 import graphic.controller.FirstGUIController;
+import java.awt.Dimension;
+
 
 public class FirstFrame extends JFrame
 {
@@ -12,5 +14,15 @@ public class FirstFrame extends JFrame
 	{
 		super();
 		this.baseController = baseController;
+		
+		setupFrame();
+	}
+	
+	//Mr H's go-to GUI helper method
+	private void setupFrame()
+	{
+		this.setSize(new Dimension(500, 500));
+		this.setVisible(true);
+		//this.setIconImage(chatbot.png);
 	}
 }
--- src/graphic/view/FirstPanel.java
+++ src/graphic/view/FirstPanel.java
@@ -1,8 +1,17 @@
 package graphic.view;
 
-import javax.swing.JFrame;
+import javax.swing.JPanel;
+import graphic.controller.FirstGUIController;
 
-public class FirstPanel
+public class FirstPanel extends JPanel
 {
+	private FirstGUIController baseController;
+	
+	public FirstPanel(FirstGUIController baseController)
+	{
+		super();
+		this.baseController = baseController;
+	}
+	
 	
 }
